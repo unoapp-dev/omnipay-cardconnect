337f6ef1a5cda1a301c3fecb2acb56bfb98ae290
Anil Ahir <anilahir@unoindia.co>
2018-09-04 16:36:42 +0530

    add optinal details for pre-authorization

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -2,6 +2,8 @@
 
 namespace Omnipay\Cardconnect\Message;
 
+use Omnipay\Cardconnect\Dictionary\CountryCodesDictionary;
+use Omnipay\Cardconnect\Dictionary\ProvinceCodesDictionary;
 use Omnipay\Common\Exception\InvalidRequestException;
 
 class AuthorizeRequest extends AbstractRequest
@@ -44,6 +46,33 @@ class AuthorizeRequest extends AbstractRequest
                         $data['expiry'] = $cardReference->expiry;
                         $data['profile'] = $cardReference->profile;
                         $data['acctid'] = $cardReference->acctid;
+    
+                        $name = $this->getCard()->getName();
+                        $address = $this->getCard()->getBillingAddress1();
+                        $city = $this->getCard()->getBillingCity();
+                        $phone = $this->getCard()->getBillingPhone();
+                        $postal = $this->getCard()->getBillingPostcode();
+                        $email = $this->getCard()->getEmail();
+    
+                        $provinceCode = $this->getCard()->getBillingState();
+                        if (strlen($provinceCode) != 2) {
+                            $provinceCode = array_search(strtolower($provinceCode), array_map('strtolower', ProvinceCodesDictionary::$codes));
+                            $provinceCode = ($provinceCode == false ? "" : $provinceCode);
+                        }
+    
+                        $counryCode = $this->getCard()->getBillingCountry();
+                        if (strlen($counryCode) != 2) {
+                            $counryCode = array_search(strtolower($counryCode), array_map('strtolower', CountryCodesDictionary::$codes));
+                        }
+                        
+                        if (!empty($name)) $data['name'] = $name;
+                        if (!empty($address)) $data['address'] = $address;
+                        if (!empty($city)) $data['city'] = $city;
+                        if (!empty($provinceCode)) $data['region'] = $provinceCode;
+                        if (!empty($counryCode)) $data['country'] = $counryCode;
+                        if (!empty($phone)) $data['phone'] = $phone;
+                        if (!empty($postal)) $data['postal'] = $postal;
+                        if (!empty($email)) $data['email'] = $email;
                     }
                     catch (\Exception $e) {
                         throw new InvalidRequestException('Invalid payment profile');
